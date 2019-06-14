6c82c0fc05a74e34d032b47e5fc31f02cd330aec
Zachary Marks <zmarks@adagetech.com>
2019-06-14 11:45:53 -0500

    Added CognitiveImageCropper plugin

---
+++ UmbracoCognitive/App_Plugins/CognitiveImageCropper/cognitiveimagecropper.prevalues.controller.js
@@ -0,0 +1,76 @@
+angular.module("umbraco").controller("Adage.PrevalueEditors.CropSizesController",
+	function ($scope) {
+
+	    if (!$scope.model.value) {
+	        $scope.model.value = [];
+	    }
+
+        $scope.editMode = false;
+        $scope.setFocus = false;
+
+	    $scope.remove = function (item, evt) {
+	        evt.preventDefault();
+	        $scope.model.value = _.reject($scope.model.value, function (x) {
+	            return x.alias === item.alias;
+	        });
+	    };
+
+	    $scope.edit = function (item, evt) {
+            evt.preventDefault();
+            $scope.editMode = true;
+            $scope.setFocus = false;
+
+	        $scope.newItem = item;
+	    };
+
+	    $scope.cancel = function (evt) {
+            evt.preventDefault();
+            $scope.editMode = false;
+            $scope.setFocus = true;
+
+	        $scope.newItem = null;
+	    };
+
+        $scope.change = function () {
+            // Listen to the change event and set focus 2 false
+            if($scope.setFocus){
+                $scope.setFocus = false;
+                return;
+            }
+        }
+
+	    $scope.add = function (evt) {
+            evt.preventDefault();
+
+            $scope.editMode = false;
+
+            $scope.setFocus = true;
+
+	        if ($scope.newItem && $scope.newItem.alias &&
+                angular.isNumber($scope.newItem.width) && angular.isNumber($scope.newItem.height) &&
+                $scope.newItem.width > 0 && $scope.newItem.height > 0) {
+
+                var exists = _.find($scope.model.value, function (item) { return $scope.newItem.alias === item.alias; });
+
+	            if (!exists) {
+	                $scope.model.value.push($scope.newItem);
+	                $scope.newItem = {};
+                    $scope.hasError = false;
+                    $scope.cropAdded = false;
+	                return;
+                }
+                else{
+                    $scope.newItem = null;
+                    $scope.hasError = false;
+                    return;
+                }
+	        }
+
+	        //there was an error, do the highlight (will be set back by the directive)
+	        $scope.hasError = true;
+        };
+
+	    $scope.sortableOptions = {
+	        axis: 'y'
+	    }
+	});
