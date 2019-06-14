410c119a843ee048ff4140bacd2c9b8ff2288cae
Mathieu Agee <magee@adagetechnologies.com>
2019-06-14 14:42:31 -0500

    Created a image uploader and package_manifest js loader

---
+++ UmbracoCognitive/App_Plugins/CognitiveImageCropper/CognitiveImageCropperConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+using Umbraco.Core.PropertyEditors;
+
+namespace UmbracoCognitive.App_Plugins.CognitiveImageCropper
+{
+    internal class CognitiveImageCropperConfiguration : ImageCropperConfiguration
+    {
+        [ConfigurationField("apiKey", "API Key", "textstring")]
+        public string ApiKey { get; set; }
+        
+    }
+}
---
+++ UmbracoCognitive/App_Plugins/CognitiveImageCropper/CognitiveImageCropperConfigurationEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Umbraco.Core.PropertyEditors;
+
+namespace UmbracoCognitive.App_Plugins.CognitiveImageCropper
+{
+    internal class CognitiveImageCropperConfigurationEditor : ConfigurationEditor<CognitiveImageCropperConfiguration>
+    {
+
+        /// <inheritdoc />
+        public override IDictionary<string, object> ToValueEditor(object configuration)
+        {
+            var d = base.ToValueEditor(configuration);
+            if (!d.ContainsKey("focalPoint")) d["focalPoint"] = new { left = 0.5, top = 0.5 };
+            if (!d.ContainsKey("src")) d["src"] = "";
+            return d;
+        }
+    }
+}
